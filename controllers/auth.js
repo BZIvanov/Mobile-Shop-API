38e05edef71dd7b7657e1e645fcb4c07b2b81350
BZIvanov <ivanov.biser@gmx.com>
2020-06-14 11:21:15 +0300

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,100 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  };
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({ success: true, token });
+};
+
+exports.register = catchAsync(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({ name, email, password, role });
+
+  sendTokenResponse(user, 200, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    return next(new AppError('Invalid credentials', 401));
+  }
+
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new AppError('Invalid credentials', 401));
+  }
+
+  sendTokenResponse(user, 200, res);
+});
+
+exports.logout = catchAsync(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ success: true, data: null });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(new AppError('User not found', 401));
+  }
+
+  req.user = currentUser;
+  next();
+});
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
---
+++ models/user.js
@@ -0,0 +1,54 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, 'Please provide a name'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ['user'],
+    default: 'user',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+schema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+schema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+schema.methods.matchPassword = async function (incomingPassword) {
+  return await bcrypt.compare(incomingPassword, this.password);
+};
+
+module.exports = mongoose.model('User', schema);
